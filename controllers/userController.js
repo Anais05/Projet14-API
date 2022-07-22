87039eda5d245ba5ffe10c2e2856360cd953f269
Anais <anaisassamoi02@gmail.com>
2022-07-22 16:59:34 +0200

    add scripts

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { ObjectId } = require('mongodb')
 
-
 module.exports.loginUser = async (req, res) => {
   let response = {}
 
